9e71cf095b2763d3b75dca637bc3644171a7df91
saivikasbilla2003 <saivikasbilla2003@gmail.com>
2025-11-30 16:27:53 -0500

    latest code from devendar

--- Test.java
+++ Test.java
@@ -1,5 +1,19 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Iteration: " + i);
+        }
+        switch (2) {
+            case 1:
+                System.out.println("Case 1");
+                break;
+            case 2:
+                System.out.println("Case 2");
+                break;
+            default:
+                System.out.println("Default case");
+        }
     }
 }
