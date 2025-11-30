375516e3ec3bcc9dea067db542931d8334f92cb7
saivikasbilla2003 <saivikasbilla2003@gmail.com>
2025-11-30 16:38:27 -0500

    deleting sys out

--- Test.java
+++ Test.java
@@ -1,10 +1,6 @@
 public class Test {
     public static void main(String[] args) {
 
-        //this is not correct code add dome differnt sysout 
-         System.out.println(" New Hello World 2026");
-   
-
         for (int i = 0; i < 5; i++) {
             System.out.println("Iteration: " + i);
         }
